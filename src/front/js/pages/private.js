d4cfea6b56e41b9a275a22fc4266cdb5d98a812a
AbsolucionArtistica <122127365+AbsolucionArtistica@users.noreply.github.com>
2023-05-26 21:23:22 -0400

    actualizacion1

---
+++ src/front/js/pages/private.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+export const Private = props => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+
+	return (
+		<div className="jumbotron">
+			<Link to="/">
+				<span className="btn btn-primary btn-lg" href="#" role="button">
+					Back home
+				</span>
+			</Link>
+		</div>
+	);
+};
